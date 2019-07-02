866c631eb96ab49f8906a65806c7e4eabc1fc745
Yusuf Sait Canbaz <yusufsaidcanbaz@gmail.comcd>
2019-07-02 18:20:53 +0300

    flash char assignment (3.) done

--- labs/lab1/2_flash_morse_char/main.cpp
+++ labs/lab1/2_flash_morse_char/main.cpp
@@ -3,33 +3,64 @@
 #include <ctype.h>
 
 Serial pc(USBTX, USBRX); // tx, rx
+DigitalOut myled(LED1);
 
 char buffer[1024];
 int charIdx = 0;
-string alphabet = "abcdefghijklmnopqrstuvwqyz";
+string alphabet = "abcdefghijklmnopqrstuvwqyz1234567890";
 string morseAlphabet[] = {".-","-...","-.-.","-..", ".", "..-.", "--.",
 
                           "....", "..", ".---", "-.-", ".-..", "--",
 
                           "-.", "---", ".--.", "--.-", ".-.", "...", "-",
 
-                          "..-", "...-", ".--", "-..-", "-.--", "--.."
+                          "..-", "...-", ".--", "-..-", "-.--", "--..",
+                          ".----", "..---", "...--", "....-", ".....", 
+                          "-....", "--...", "---..", "----.", "-----"
                          };
 
-void flashChar(char c)
+void flashLed(float time)
+{
+    myled = 1;
+    wait(time);
+    myled = 0;
+    wait(time);
+}
+
+string char2morse(char c)
 {
     c = tolower(c);
     int index = alphabet.find(c);
 
     if(index != -1) {
-        return morse[index];
+        return morseAlphabet[index];
     } else {
         return " ";
     }
 }
 
-void flashMorse(string morseChar) {
-    morseChar.le    
+void flashMorse(string morseChar)
+{
+    if (morseChar == " ") {
+        return;
+    }
+    int len = morseChar.size();
+    for (int i = 0; i < len; i++) {
+        char ch = morseChar[i];
+        if (ch == '-') {
+            flashLed(1.5);
+        } else if (ch == '.') {
+            flashLed(0.5);
+        } else {
+            pc.printf("line 46 not dot or dash in morse string");
+        }
+    }
+}
+
+void flashCharInMorse(char c)
+{
+    string morseChar = char2morse(c);
+    flashMorse(morseChar);
 }
 
 int main()
@@ -37,15 +68,6 @@ int main()
     pc.printf("Hello World!\n\r");
     while(1) {
         char c = pc.getc();
-
-        if (c == '\n' || c == '\r') {
-            pc.printf(buffer);
-            pc.printf("\n");
-            charIdx = 0;
-            memset(buffer, 0, sizeof buffer);
-        } else {
-            buffer[charIdx] = c;
-            charIdx++;
-        }
+        flashCharInMorse(c);
     }
 }
