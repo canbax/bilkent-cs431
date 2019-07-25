ab1a68ec1532348520b3e1cc80707007ad764f9f
yusuf canbaz <yusufsaidcanbaz@gmail.com>
2019-07-25 21:28:40 +0300

    part 6 circular buffer used

--- labs/lab2/6/main.cpp
+++ labs/lab2/6/main.cpp
@@ -15,25 +15,25 @@ const float NOISE_SAMPLING_PERIOD = 1.0;
 
 unsigned int noiseBuffer[NOISE_BUFFER_SIZE];
 int noiseBufferIdx = 0;
+bool isNoiseBufferFulled = false;
 
 char chBuffer[BUFFER_SIZE];
+char currChar;
 int chBufferIdx = 0;
 
 Ticker ticker;
 
-void tickTock() {
-  led = !led;
-  noiseBufferIdx++;
-  if (noiseBufferIdx == NOISE_BUFFER_SIZE) {
-    noiseBufferIdx = 0;
-  }
-}
-
 void dumpNoiseIfCommanded(string inp) {
   if (inp.find(STR_DUMP) != string::npos) {
     pc.printf("\n");
-    for (int i = 0; i < noiseBufferIdx; i++) {
+
+    for (int i = noiseBufferIdx; i >= 0; i--) {
       pc.printf("%d", noiseBuffer[i]);
+    }
+    if (isNoiseBufferFulled) {
+      for (int i = NOISE_BUFFER_SIZE - 1; i > noiseBufferIdx; i--) {
+        pc.printf("%d", noiseBuffer[i]);
+      }
     }
     pc.printf("\n");
   }
@@ -44,25 +44,33 @@ void noiseOccured() {
   noiseBuffer[noiseBufferIdx] = 1;
 }
 
+void tickTock() {
+  led = !led;
+  noiseBufferIdx++;
+  noiseBuffer[noiseBufferIdx] = 0;
+  if (noiseBufferIdx == NOISE_BUFFER_SIZE) {
+    isNoiseBufferFulled = true;
+    noiseBufferIdx = 0;
+  }
+}
+
 int main() {
   pc.printf("Hello World!\n\r");
   ticker.attach(&tickTock, NOISE_SAMPLING_PERIOD);
   noiseInterrupt.rise(&noiseOccured);
+  // pc.attach(&keyPressed, Serial::RxIrq);
 
   // spin in a main loop
   while (1) {
-    char c = pc.getc();
-
-    if (c == '\n' || c == '\r') {
+    char ch = pc.getc();
+    if (ch == '\n' || ch == '\r') {
       pc.printf(chBuffer);
       pc.printf("\n");
       dumpNoiseIfCommanded(chBuffer);
       chBufferIdx = 0;
-      noiseBufferIdx = 0;
       memset(chBuffer, 0, sizeof chBuffer);
-      memset(noiseBuffer, 0, sizeof noiseBuffer);
     } else {
-      chBuffer[chBufferIdx] = c;
+      chBuffer[chBufferIdx] = ch;
       chBufferIdx++;
     }
   }
